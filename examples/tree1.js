3d2154935bfe2661b37c42e76371a15f69ef0421
Cheton Wu <cheton@gmail.com>
2016-03-27 19:51:51 +0800

    Show the total number of child nodes

--- examples/tree1.js
+++ examples/tree1.js
@@ -4,11 +4,12 @@ import { flatten } from '../src';
 import tree from '../test/fixtures/tree.json';
 
 flatten(tree, { openAllNodes: true }).forEach((node, index) => {
-    console.log('%s: path=%s, parent=%s, children=%d, depth=%d, prefix=%s, last=%d, more=%d, open=%d',
+    console.log('%s: path=%s, parent=%s, children=%d, total=%d, depth=%d, prefix=%s, last=%d, more=%d, open=%d',
         node.label,
         JSON.stringify(node.state.path),
         node.parent !== null ? JSON.stringify(node.parent.state.path) : null,
         Object.keys(node.children).length,
+        node.state.total,
         node.state.depth,
         JSON.stringify(node.state.prefixMask),
         node.state.last,
