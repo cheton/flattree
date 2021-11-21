2be8308752226797f0814f90593777a1f92f7d2d
Cheton Wu <cheton@gmail.com>
2021-11-21 21:53:33 +0800

    feat: setup CI/CD workflow with GitHub Actions

--- src/flatten.js
+++ src/flatten.js
@@ -8,173 +8,173 @@ import Node from './node';
 // @param {array} [options.openNodes] An array that contains the ids of open nodes.
 // @return {array}
 const flatten = (nodes = [], options = {}) => {
-    nodes = [].concat(nodes);
+  nodes = [].concat(nodes);
 
-    const flatten = [];
-    const stack = [];
-    const pool = {
-        lastChild: {}
-    };
+  const flatten = [];
+  const stack = [];
+  const pool = {
+    lastChild: {}
+  };
 
-    options.openAllNodes = !!options.openAllNodes;
-    options.openNodes = options.openNodes || [];
-    options.throwOnError = !!options.throwOnError;
+  options.openAllNodes = !!options.openAllNodes;
+  options.openNodes = options.openNodes || [];
+  options.throwOnError = !!options.throwOnError;
 
-    { // root node
-        let firstNode = (nodes.length > 0) ? nodes[0] : null;
-        let parentNode = firstNode ? firstNode.parent : null;
-        if (parentNode && !(parentNode instanceof Node)) {
-            parentNode = new Node(parentNode);
-        }
-        const rootNode = parentNode || new Node({ // defaults
-            parent: null,
-            children: nodes,
-            state: {
-                depth: -1,
-                open: true, // always open
-                path: '',
-                prefixMask: '',
-                total: 0
-            }
-        });
+  { // root node
+    let firstNode = (nodes.length > 0) ? nodes[0] : null;
+    let parentNode = firstNode ? firstNode.parent : null;
+    if (parentNode && !(parentNode instanceof Node)) {
+      parentNode = new Node(parentNode);
+    }
+    const rootNode = parentNode || new Node({ // defaults
+      parent: null,
+      children: nodes,
+      state: {
+        depth: -1,
+        open: true, // always open
+        path: '',
+        prefixMask: '',
+        total: 0
+      }
+    });
 
-        if (rootNode === parentNode) {
-            const subtotal = (rootNode.state.total || 0);
+    if (rootNode === parentNode) {
+      const subtotal = (rootNode.state.total || 0);
 
-            // Traversing up through its ancestors.
-            let p = rootNode;
-            while (p) {
-                const { path, total = 0 } = p.state;
+      // Traversing up through its ancestors.
+      let p = rootNode;
+      while (p) {
+        const { path, total = 0 } = p.state;
 
-                // Rebuild the lastChild pool.
-                if (p.isLastChild() && path) {
-                    pool.lastChild[path] = true;
-                }
-
-                // Subtract the number 'subtotal' from the total of the root node and all its ancestors.
-                p.state.total = (total - subtotal);
-                if (p.state.total < 0) {
-                    if (options.throwOnError) {
-                        throw new Error('The node might have been corrupted: id=' + JSON.stringify(p.id) + ', state=' + JSON.stringify(p.state));
-                    } else {
-                        console && console.log('Error: The node might have been corrupted: id=%s, parent=%s, children=%s, state=%s',
-                            JSON.stringify(p.id),
-                            p.parent,
-                            p.children,
-                            JSON.stringify(p.state),
-                        );
-                    }
-                }
-
-                p = p.parent;
-            }
+        // Rebuild the lastChild pool.
+        if (p.isLastChild() && path) {
+          pool.lastChild[path] = true;
         }
 
-        stack.push([rootNode, rootNode.state.depth, 0]);
+        // Subtract the number 'subtotal' from the total of the root node and all its ancestors.
+        p.state.total = (total - subtotal);
+        if (p.state.total < 0) {
+          if (options.throwOnError) {
+            throw new Error('The node might have been corrupted: id=' + JSON.stringify(p.id) + ', state=' + JSON.stringify(p.state));
+          } else {
+            console && console.log('Error: The node might have been corrupted: id=%s, parent=%s, children=%s, state=%s',
+              JSON.stringify(p.id),
+              p.parent,
+              p.children,
+              JSON.stringify(p.state),
+            );
+          }
+        }
+
+        p = p.parent;
+      }
     }
 
-    while (stack.length > 0) {
-        let [current, depth, index] = stack.pop();
+    stack.push([rootNode, rootNode.state.depth, 0]);
+  }
 
-        while (index < current.children.length) {
-            let node = current.children[index];
-            if (!(node instanceof Node)) {
-                node = new Node(node);
-            }
-            node.parent = current;
-            node.children = node.children || [];
+  while (stack.length > 0) {
+    let [current, depth, index] = stack.pop();
 
-            // Ensure parent.children[index] is equal to the current node.
-            node.parent.children[index] = node;
+    while (index < current.children.length) {
+      let node = current.children[index];
+      if (!(node instanceof Node)) {
+        node = new Node(node);
+      }
+      node.parent = current;
+      node.children = node.children || [];
 
-            const path = current.state.path + '.' + index;
-            const open = node.hasChildren() && (() => {
-                const { openAllNodes, openNodes } = options;
-                if (openAllNodes) {
-                    return true;
-                }
-                // determine from input
-                if (node.state && node.state.open) {
-                    return true;
-                }
-                // determine by node object
-                if (openNodes.indexOf(node) >= 0) {
-                    return true;
-                }
-                // determine by node id
-                if (openNodes.indexOf(node.id) >= 0) {
-                    return true;
-                }
-                return false;
-            })();
-            const prefixMask = ((prefix) => {
-                let mask = '';
-                while (prefix.length > 0) {
-                    prefix = prefix.replace(/\.\d+$/, '');
-                    if (!prefix || pool.lastChild[prefix]) {
-                        mask = '0' + mask;
-                    } else {
-                        mask = '1' + mask;
-                    }
-                }
-                return mask;
-            })(path);
+      // Ensure parent.children[index] is equal to the current node.
+      node.parent.children[index] = node;
 
-            if (index === (current.children.length - 1)) {
-                // The node is the last child of its parent.
-                pool.lastChild[path] = true;
-            }
-
-            // This allows you to put extra information to node.state
-            node.state = extend({}, node.state, {
-                depth: depth + 1,
-                open: open,
-                path: path,
-                prefixMask: prefixMask,
-                total: 0
-            });
-
-            let parentDidOpen = true;
-
-            { // Check the open state from its ancestors.
-                let p = node;
-                while (p.parent !== null) {
-                    if (p.parent.state.open === false) {
-                        parentDidOpen = false;
-                        break;
-                    }
-                    p = p.parent;
-                }
-            }
-
-            if (parentDidOpen) {
-                // Push the node to flatten list only if all of its parent nodes have the open state set to true.
-                flatten.push(node);
-
-                // Update the total number of visible child nodes.
-                let p = node;
-                while (p.parent !== null) {
-                    p.parent.state.total++;
-                    p = p.parent;
-                }
-            }
-
-            ++index;
-
-            if (node.hasChildren()) {
-                // Push back parent node to the stack that will be able to continue
-                // the next iteration once all the child nodes of the current node
-                // have been completely explored.
-                stack.push([current, depth, index]);
-
-                index = 0;
-                depth = depth + 1;
-                current = node;
-            }
+      const path = current.state.path + '.' + index;
+      const open = node.hasChildren() && (() => {
+        const { openAllNodes, openNodes } = options;
+        if (openAllNodes) {
+          return true;
         }
-    }
+        // determine from input
+        if (node.state && node.state.open) {
+          return true;
+        }
+        // determine by node object
+        if (openNodes.indexOf(node) >= 0) {
+          return true;
+        }
+        // determine by node id
+        if (openNodes.indexOf(node.id) >= 0) {
+          return true;
+        }
+        return false;
+      })();
+      const prefixMask = ((prefix) => {
+        let mask = '';
+        while (prefix.length > 0) {
+          prefix = prefix.replace(/\.\d+$/, '');
+          if (!prefix || pool.lastChild[prefix]) {
+            mask = '0' + mask;
+          } else {
+            mask = '1' + mask;
+          }
+        }
+        return mask;
+      })(path);
 
-    return flatten;
+      if (index === (current.children.length - 1)) {
+        // The node is the last child of its parent.
+        pool.lastChild[path] = true;
+      }
+
+      // This allows you to put extra information to node.state
+      node.state = extend({}, node.state, {
+        depth: depth + 1,
+        open: open,
+        path: path,
+        prefixMask: prefixMask,
+        total: 0
+      });
+
+      let parentDidOpen = true;
+
+      { // Check the open state from its ancestors.
+        let p = node;
+        while (p.parent !== null) {
+          if (p.parent.state.open === false) {
+            parentDidOpen = false;
+            break;
+          }
+          p = p.parent;
+        }
+      }
+
+      if (parentDidOpen) {
+        // Push the node to flatten list only if all of its parent nodes have the open state set to true.
+        flatten.push(node);
+
+        // Update the total number of visible child nodes.
+        let p = node;
+        while (p.parent !== null) {
+          p.parent.state.total++;
+          p = p.parent;
+        }
+      }
+
+      ++index;
+
+      if (node.hasChildren()) {
+        // Push back parent node to the stack that will be able to continue
+        // the next iteration once all the child nodes of the current node
+        // have been completely explored.
+        stack.push([current, depth, index]);
+
+        index = 0;
+        depth += 1;
+        current = node;
+      }
+    }
+  }
+
+  return flatten;
 };
 
 export default flatten;
--- src/node.js
+++ src/node.js
@@ -1,101 +1,101 @@
 import extend from './extend';
 
 class Node {
-    id = null;
-    parent = null;
-    children = [];
-    state = {};
+  id = null;
+  parent = null;
+  children = [];
+  state = {};
 
-    constructor(node) {
-        extend(this, node);
+  constructor(node) {
+    extend(this, node);
 
-        this.children = this.children || [];
+    this.children = this.children || [];
+  }
+  // Returns a boolean value indicating whether a node is a descendant of a given node or not.
+  // @param {object} node Specifies the node that may be contained by (a descendant of) a specified node.
+  // @return {boolean} Returns true if a node is a descendant of a specified node, otherwise false. A descendant can be a child, grandchild, great-grandchild, and so on.
+  contains(node) {
+    while ((node instanceof Node) && (node !== this)) {
+      if (node.parent === this) {
+        return true;
+      }
+      node = node.parent;
     }
-    // Returns a boolean value indicating whether a node is a descendant of a given node or not.
-    // @param {object} node Specifies the node that may be contained by (a descendant of) a specified node.
-    // @return {boolean} Returns true if a node is a descendant of a specified node, otherwise false. A descendant can be a child, grandchild, great-grandchild, and so on.
-    contains(node) {
-        while ((node instanceof Node) && (node !== this)) {
-            if (node.parent === this) {
-                return true;
-            }
-            node = node.parent;
-        }
-        return false;
+    return false;
+  }
+  // Gets a child node at the specified index.
+  // @param {number} The index of the child node.
+  // @return {object} Returns an object that defines the node, null otherwise.
+  getChildAt(index) {
+    let node = null;
+    if ((this.children.length > 0) && (index >= 0) && (index < this.children.length)) {
+      node = this.children[index];
     }
-    // Gets a child node at the specified index.
-    // @param {number} The index of the child node.
-    // @return {object} Returns an object that defines the node, null otherwise.
-    getChildAt(index) {
-        let node = null;
-        if ((this.children.length > 0) && (index >= 0) && (index < this.children.length)) {
-            node = this.children[index];
-        }
-        return node;
+    return node;
+  }
+  // Gets the child nodes.
+  // @return {array} Returns an array of objects that define the nodes.
+  getChildren() {
+    return this.children;
+  }
+  // Gets the first child node.
+  // @return {object} Returns an object that defines the node, null otherwise.
+  getFirstChild() {
+    let node = null;
+    if (this.children.length > 0) {
+      node = this.children[0];
     }
-    // Gets the child nodes.
-    // @return {array} Returns an array of objects that define the nodes.
-    getChildren() {
-        return this.children;
+    return node;
+  }
+  // Gets the last child node.
+  // @return {object} Returns an object that defines the node, null otherwise.
+  getLastChild() {
+    let node = null;
+    if (this.children.length > 0) {
+      node = this.children[this.children.length - 1];
     }
-    // Gets the first child node.
-    // @return {object} Returns an object that defines the node, null otherwise.
-    getFirstChild() {
-        let node = null;
-        if (this.children.length > 0) {
-            node = this.children[0];
-        }
-        return node;
+    return node;
+  }
+  // Gets the next sibling node.
+  // @return {object} Returns an object that defines the node, null otherwise.
+  getNextSibling() {
+    let node = null;
+    if (this.parent) {
+      const index = this.parent.children.indexOf(this);
+      if ((index >= 0) && (index < this.parent.children.length - 1)) {
+        node = this.parent.children[index + 1];
+      }
     }
-    // Gets the last child node.
-    // @return {object} Returns an object that defines the node, null otherwise.
-    getLastChild() {
-        let node = null;
-        if (this.children.length > 0) {
-            node = this.children[this.children.length - 1];
-        }
-        return node;
+    return node;
+  }
+  // Gets the parent node.
+  // @return {object} Returns an object that defines the node, null otherwise.
+  getParent() {
+    return this.parent;
+  }
+  // Gets the previous sibling node.
+  // @return {object} Returns an object that defines the node, null otherwise.
+  getPreviousSibling() {
+    let node = null;
+    if (this.parent) {
+      const index = this.parent.children.indexOf(this);
+      if ((index > 0) && (index < this.parent.children.length)) {
+        node = this.parent.children[index - 1];
+      }
     }
-    // Gets the next sibling node.
-    // @return {object} Returns an object that defines the node, null otherwise.
-    getNextSibling() {
-        let node = null;
-        if (this.parent) {
-            const index = this.parent.children.indexOf(this);
-            if ((index >= 0) && (index < this.parent.children.length - 1)) {
-                node = this.parent.children[index + 1];
-            }
-        }
-        return node;
-    }
-    // Gets the parent node.
-    // @return {object} Returns an object that defines the node, null otherwise.
-    getParent() {
-        return this.parent;
-    }
-    // Gets the previous sibling node.
-    // @return {object} Returns an object that defines the node, null otherwise.
-    getPreviousSibling() {
-        let node = null;
-        if (this.parent) {
-            const index = this.parent.children.indexOf(this);
-            if ((index > 0) && (index < this.parent.children.length)) {
-                node = this.parent.children[index - 1];
-            }
-        }
-        return node;
-    }
-    // Checks whether this node has children.
-    // @return {boolean} Returns true if the node has children, false otherwise.
-    hasChildren() {
-        return this.children.length > 0;
-    }
-    // Checks whether this node is the last child of its parent.
-    // @return {boolean} Returns true if the node is the last child of its parent, false otherwise.
-    isLastChild() {
-        const hasNextSibling = this.getNextSibling();
-        return !hasNextSibling;
-    }
+    return node;
+  }
+  // Checks whether this node has children.
+  // @return {boolean} Returns true if the node has children, false otherwise.
+  hasChildren() {
+    return this.children.length > 0;
+  }
+  // Checks whether this node is the last child of its parent.
+  // @return {boolean} Returns true if the node is the last child of its parent, false otherwise.
+  isLastChild() {
+    const hasNextSibling = this.getNextSibling();
+    return !hasNextSibling;
+  }
 }
 
 export default Node;
